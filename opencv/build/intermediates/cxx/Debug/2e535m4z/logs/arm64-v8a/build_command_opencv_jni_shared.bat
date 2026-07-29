b68e57d0d620aaf9feca86c590082f79d450da5f
Antek <117514243+SI4R3K@users.noreply.github.com>
2026-07-29 19:22:48 +0200

    Delete opencv directory

--- opencv/build/intermediates/cxx/Debug/2e535m4z/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\asus\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\asus\\AndroidStudioProjects\\VisuallyImparedSupportApp\\opencv\\.cxx\\Debug\\2e535m4z\\arm64-v8a" ^
-  opencv_jni_shared
